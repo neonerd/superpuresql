d7b831b6cc39269badc0eb89b7d74893e999ff93
Andrej Sykora <as@andrejsykora.com>
2017-04-22 12:11:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const puresql = require('puresql')
+const args = require('yargs').argv
+
+console.log('The parameters are', args)
+
+// Dialects
+
+// Database parsing
+async function getDbStructure (dialect, adapter) {
+
+}
+
+function init () {
+
+}
+
+/**
+Exports a programatic access
+*/
+module.exports = init
